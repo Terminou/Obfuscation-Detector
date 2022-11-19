126e18eb13c7f7af6005bf6cbe60fc77af9ace79
Eren Kilic <erenkilic332@gmail.com>
2022-11-20 00:32:04 +0300

    Tokenized data are saved.

--- Feature Extractor/index.js
+++ Feature Extractor/index.js
@@ -5,6 +5,7 @@ var esprima = require('esprima');
 const fs = require('fs');
 
 var inputPath = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesNotObfuscated/JavascriptSamples/';
+var writePath = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesNotObfuscated/AST/';
 
 //Get all input names
 var inputNameList = fs.readdirSync(inputPath);
@@ -12,6 +13,7 @@ for (let i = 0; i < inputNameList.length; i++) {
     const data = fs.readFileSync(inputPath + inputNameList[i], 'utf8');
     try {
         var result = esprima.tokenize(data);
+        fs.writeFileSync(writePath + inputNameList[i], result.toString, 'utf-8');
     } catch (error) {
         console.log("Error!");
     }
