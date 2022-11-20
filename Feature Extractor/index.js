9a8dddb5d57c4a0b030d13b029bdc8470fd6069b
Eren Kilic <erenkilic332@gmail.com>
2022-11-20 18:58:13 +0300

    AST of dataset found and written into a folder.

--- Feature Extractor/index.js
+++ Feature Extractor/index.js
@@ -5,7 +5,7 @@ var esprima = require('esprima');
 const fs = require('fs');
 
 var inputPath = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesNotObfuscated/JavascriptSamples/';
-var writePath = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesNotObfuscated/AST/';
+var writePath = '/Users/erenkilic/Desktop/Obfuscation Detector/AST/';
 
 //Get all input names
 var inputNameList = fs.readdirSync(inputPath);
@@ -13,7 +13,9 @@ for (let i = 0; i < inputNameList.length; i++) {
     const data = fs.readFileSync(inputPath + inputNameList[i], 'utf8');
     try {
         var result = esprima.tokenize(data);
-        fs.writeFileSync(writePath + inputNameList[i], result.toString, 'utf-8');
+        let jsonString = JSON.stringify(result);
+        let outName = inputNameList[i].substring(0, inputNameList[i].indexOf('.js')) + '.ast';
+        fs.writeFileSync(writePath + outName, jsonString, 'utf-8');
     } catch (error) {
         console.log("Error!");
     }
