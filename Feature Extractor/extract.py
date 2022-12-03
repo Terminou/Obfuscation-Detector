92896590c0abc434b8ef7bad70508b9d7abfc7cc
Eren Kilic <erenkilic332@gmail.com>
2022-12-03 18:46:51 +0300

    Read the name of raw dataset from file system.

--- Feature Extractor/extract.py
+++ Feature Extractor/extract.py
@@ -1,1 +1,8 @@
+import os
 print("Extracting features using AST outputs...")
+
+o_path = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesNotObfuscated/JavascriptSamples/'
+no_path = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesObfuscated/JavascriptSamplesObfuscated/'
+
+not_obfuscated_raw_list = os.listdir(no_path)
+obfuscated_raw_list = os.listdir(o_path)
