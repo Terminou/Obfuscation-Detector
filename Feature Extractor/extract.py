29c369082480662a314c207ce2ef5f626afb8f7a
Eren Kilic <erenkilic332@gmail.com>
2022-12-03 21:18:47 +0300

    Total number of lines function added.

--- Feature Extractor/extract.py
+++ Feature Extractor/extract.py
@@ -1,8 +1,19 @@
 import os
-print("Extracting features using AST outputs...")
 
 o_path = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesNotObfuscated/JavascriptSamples/'
 no_path = '/Users/erenkilic/Desktop/Obfuscation Detector/Dataset/JavascriptSamplesObfuscated/JavascriptSamplesObfuscated/'
 
+
+def total_number_of_lines(not_obfuscated_raw_list):
+    for data in not_obfuscated_raw_list:
+        f = open(no_path + data)
+        print(f.read())
+
+
+print("Extracting features using AST outputs...")
+
+
 not_obfuscated_raw_list = os.listdir(no_path)
 obfuscated_raw_list = os.listdir(o_path)
+
+total_number_of_lines(not_obfuscated_raw_list)
