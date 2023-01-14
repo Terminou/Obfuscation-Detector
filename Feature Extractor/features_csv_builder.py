9a43b63e184abd414fec98ef4672681d65f10581
Terminou <erenkilic332@gmail.com>
2023-01-14 16:33:00 +0300

    Added F16 to builder class.

--- Feature Extractor/features_csv_builder.py
+++ Feature Extractor/features_csv_builder.py
@@ -11,7 +11,7 @@ files = sorted(os.listdir(directory), key=str.casefold)[1:]
 
 # Build a list of strings from F1 to F46
 fields = []
-for i in range(1, 47):
+for i in range(1, 48):
     fields.append('F{}'.format(i))
 fields.append('Class')
 
@@ -40,7 +40,7 @@ with open('features.csv', 'w', newline='') as csv_file:
                 avg_nr_args_per_function(input_file),
                 nr_of_func_definitions_ratio(input_file),
                 nr_of_special_js_elements_ratio(input_file),
-                #TODO: Feature 16 - Number of renamed special JavaScript elements divided by F3
+                renamed_js_elements_ratio(input_file),
                 encoded_chars_ratio(input_file),
                 backslash_chars_ratio(input_file),
                 pipe_chars_ratio(input_file),
@@ -108,7 +108,7 @@ with open('features.csv', 'a', newline='') as csv_file:
                     avg_nr_args_per_function(input_file),
                     nr_of_func_definitions_ratio(input_file),
                     nr_of_special_js_elements_ratio(input_file),
-                    #TODO: Feature 16 - Number of renamed special JavaScript elements divided by F3
+                    renamed_js_elements_ratio(input_file),
                     encoded_chars_ratio(input_file),
                     backslash_chars_ratio(input_file),
                     pipe_chars_ratio(input_file),
